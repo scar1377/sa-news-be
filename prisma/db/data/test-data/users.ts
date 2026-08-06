7ceddbc225e1b22407583511661b318c6b693413
scar1377 <scarlettadams77@gmail.com>
2026-08-06 19:15:27 +0100

    replace test user raw data with the correct data

--- prisma/db/data/test-data/users.ts
+++ prisma/db/data/test-data/users.ts
@@ -1,38 +1,24 @@
 export const users = [
   {
-    username: "tickle122",
-    name: "Tom Tickle",
+    username: "butter_bridge",
+    name: "jonny",
     avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+      "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
   },
   {
-    username: "grumpy19",
-    name: "Paul Grump",
-    avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
+    username: "icellusedkars",
+    name: "sam",
+    avatar_url: "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
   },
   {
-    username: "happyamy2016",
-    name: "Amy Happy",
-    avatar_url:
-      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
+    username: "rogersop",
+    name: "paul",
+    avatar_url: "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
   },
   {
-    username: "cooljmessy",
-    name: "Peter Messy",
+    username: "lurker",
+    name: "do_nothing",
     avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
-  },
-  {
-    username: "weegembump",
-    name: "Gemma Bump",
-    avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
-  },
-  {
-    username: "jessjelly",
-    name: "Jess Jelly",
-    avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+      "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
   },
 ];
