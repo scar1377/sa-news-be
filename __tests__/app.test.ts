4eac1e310ba35c961f295262719e4d4fff70772e
scar1377 <scarlettadams77@gmail.com>
2026-08-06 19:14:43 +0100

    set up supertest and the test file for the endpoints

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,7 +1,17 @@
 import request from "supertest";
-import { describe, test, expect } from "vitest";
-// import app from "../src/app";
-// import { testData } from "../prisma/db/data/test-data";
-// import { seed } from "../prisma/db/seed";
+import { describe, test, expect, beforeEach, afterAll } from "vitest";
+import app from "../src/app";
+import { testData } from "../prisma/db/data/test-data";
+import { seed } from "../prisma/db/seed";
+import { prisma } from "../prisma/db/connection";
 
-describe("GET ");
+beforeEach(async () => await seed(prisma, testData));
+
+describe("app", () => {
+  describe("api/topic", () => {
+    test("GET api/topics - status 200", async () => {
+      const res = await request(app).get("/api/topics");
+      expect(res.status).toBe(200);
+    });
+  });
+});
