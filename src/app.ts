013ee080b5bf9faf36364c3083423b8945499e07
scar1377 <scarlettadams77@gmail.com>
2026-08-08 21:38:03 +0100

    add 500 internal server error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { topicsRouter } from "./routers/topics.router";
 import { articlesRouter } from "./routers/articles.router";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
 
@@ -10,5 +11,10 @@ app.all("/{*invalidPath}", (req, res) => {
   res.status(404).send({
     msg: "Path not found",
   });
+});
+
+app.use((err: Error, req: Request, res: Response) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal service error" });
 });
 export default app;
