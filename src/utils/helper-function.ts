71a85e7663f68a03d7d8e27e246090d77f7f41f2
scar1377 <scarlettadams77@gmail.com>
2026-08-08 22:49:12 +0100

    add helper function isCustomeError

---
+++ src/utils/helper-function.ts
@@ -0,0 +1,13 @@
+import { CustomError } from "../types/api.types";
+
+export const isCustomError = (error: unknown): error is CustomError => {
+  if (typeof error !== "object" || error === null) {
+    return false;
+  }
+
+  if (!("status" in error) || !("msg" in error)) {
+    return false;
+  }
+
+  return typeof error.status === "number" && typeof error.msg === "string";
+};
