82d33bf2d9593cbcbd4a42c17b31863825bfa491
scar1377 <scarlettadams77@gmail.com>
2026-08-08 22:51:17 +0100

    add custom error handler

---
+++ src/controllers/errors.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { isCustomError } from "../utils/helper-function";
+
+export const customErrorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (isCustomError(err)) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+};
+
+export const internalServerErrorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  res.status(500).send({ msg: "Internal service error" });
+};
