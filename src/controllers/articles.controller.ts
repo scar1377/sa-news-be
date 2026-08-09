92d1474077a48719e6486d6e886f6fd71fa64ae2
scar1377 <scarlettadams77@gmail.com>
2026-08-09 22:53:36 +0100

    complete sad path 400 article_id is a decimal for GET /api/articles/:article_id

--- src/controllers/articles.controller.ts
+++ src/controllers/articles.controller.ts
@@ -21,12 +21,13 @@ export const getArticleById = (
   const { article_id } = req.params;
   const parsedArticle_id = Number(article_id);
 
-  if (Number.isNaN(parsedArticle_id)) {
+  if (Number.isNaN(parsedArticle_id) || !Number.isInteger(parsedArticle_id)) {
     return next({
       status: 400,
       msg: "Bad request - invalid article_id",
     });
   }
+
   selectArticlesById(parsedArticle_id)
     .then((article) => {
       res.status(200).send({ article });
