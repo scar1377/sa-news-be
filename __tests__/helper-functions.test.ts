9a3d2a8b2fe145a50e57d9ee084440c00b27d2e5
scar1377 <scarlettadams77@gmail.com>
2026-08-10 15:02:26 +0100

    get isValidId fully tested

--- __tests__/helper-functions.test.ts
+++ __tests__/helper-functions.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { isCustomError } from "../src/utils/helper-function";
+import { isCustomError, isValidId } from "../src/utils/helper-function";
 
 describe("isCustomError", () => {
   test("returns false when pass null", () => {
@@ -24,5 +24,23 @@ describe("isCustomError", () => {
   });
   test("returns true when pass an error object with both status and msg properties", () => {
     expect(isCustomError({ status: 404, msg: "error not found" })).toBe(true);
+  });
+});
+
+describe("isValidId", () => {
+  test("returns true if the id is 0", () => {
+    expect(isValidId(0)).toBe(true);
+  });
+  test("returns true if the id is an integer", () => {
+    expect(isValidId(1)).toBe(true);
+  });
+  test("returns true if the id is a negative integer", () => {
+    expect(isValidId(-1)).toBe(true);
+  });
+  test("returns false if the id is NaN", () => {
+    expect(isValidId(NaN)).toBe(false);
+  });
+  test("returns false if the id is not an integer", () => {
+    expect(isValidId(1.5)).toBe(false);
   });
 });
